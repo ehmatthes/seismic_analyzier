7a7b1ae74d9dabec5dc61c2660184403597af7e5
Eric Matthes <ehmatthes@gmail.com>
2024-05-02 07:24:39 -0800

    Generates series of plots focusing on Kramer Ave event, 8/18/2015.

---
+++ kramer_plots.py
@@ -0,0 +1,23 @@
+"""Generate a series of plots focusing on the Kramer Ave slide, 8/18/2015."""
+
+from pathlib import Path
+
+import obspy
+
+
+data_dir = Path(__file__).parent / "data" / "kramer_08182015"
+data_files = data_dir.glob("*.mseed")
+
+output_dir = Path(__file__).parent / "output"
+
+for file in data_files:
+    output_file = "kramer_" + file.name.removesuffix(".mseed") + ".png"
+    output_path = output_dir / output_file
+    print("Writing file:", output_path)
+
+    stream = obspy.read(file)
+    stream.plot(outfile=output_path)
+
+    # Pause and view a specific plot:
+    # if "08182015" in file.name:
+    #     stream.plot()
